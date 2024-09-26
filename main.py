8bfb926aa08065d153a4cbdbc47a6e8bdb859dbf
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 17:11:20 +0200

    main update

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from api import Api
 from quiz import Question
 
+def main():
 
-initialize_api = Api()
-questions = initialize_api.quiz_request()
-quiz_list = []
-for item in questions:
-    quiz_list.append(item)
+    initialize_api = Api()
+    questions = initialize_api.quiz_request()
+    quiz_list = []
+
+    for item in questions:
+        quiz_list.append(item)
 
 
-initialize_question = Question()
-test = initialize_question.test(quiz_list)
-print(test)
+    initialize_question = Question()
+    test = initialize_question.test(quiz_list)
+    print(test)
+
 
 if __name__ == "__main__":
     main()
