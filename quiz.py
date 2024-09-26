0af3399da8a14ec88e2ea3a0df1d385d28ae98fe
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 17:12:35 +0200

    game and check_answer method, not done

--- quiz.py
+++ quiz.py
@@ -1,12 +1,26 @@
 from api import Api
-
 class Question:
+
     def __init__(self):
         self.question_number = 0
-        self.correct_answer = 0
+        self.user_score = 0
     
-    def test(self, question):
-        return question
+
+    def game(self, question):
+        while question_number < len(question):
+            text = question[self.question_number]["question"]
+            correct_answer = question[self.question_number]["correct_answer"]
+            self.question_number += 1
+            user_input = input(f"Q.{self.question_number}")
+            check_answer(user_answer, correct_answer)
+
+
+    def check_answer(self, user_asnwer, correct_answer):
+        if check_answer.capitalize() == correct_answer:
+            self.user_score += 1
+        else:
+            return f"Sorry, wrong answer.\nScore: {self.user_score} / {self.question_number}"
+    
 
 
 if __name__ == "__main__":
