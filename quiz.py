fa12a3e6a0b619c22db7816a6740b5d5fe68e3c4
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 17:28:52 +0200

    Fixed misstypo, added formatting for better user readability

--- quiz.py
+++ quiz.py
@@ -1,27 +1,34 @@
 from api import Api
+
+
 class Question:
 
     def __init__(self):
         self.question_number = 0
         self.user_score = 0
-    
 
     def game(self, question):
+        print("Welcome! Please answer the questions with True or False\n")
         while self.question_number < len(question):
             text = question[self.question_number]["question"]
             correct_answer = question[self.question_number]["correct_answer"]
             self.question_number += 1
-            user_input = input(f"Q.{self.question_number}: {text}")
-            check_answer(user_input, correct_answer)
+            user_input = input(f"Q.{self.question_number}: {text}\nAnswer: ")
+            self.check_answer(user_input, correct_answer)
+        print(
+            f"Game is over!\nYour final score is: {self.user_score} / {self.question_number}"
+        )
 
-
-    def check_answer(self, user_asnwer, correct_answer):
+    def check_answer(self, user_answer, correct_answer):
         if user_answer.capitalize() == correct_answer:
             self.user_score += 1
-            return f"Good job! Score: {self.user_score} / {self.question_number}"
+            print(
+                f"Good job!\nScore: {self.user_score} / {self.question_number}\n"
+            )
         else:
-            return f"Sorry, wrong answer.\nScore: {self.user_score} / {self.question_number}"
-    
+            print(
+                f"Sorry, wrong answer.\nScore: {self.user_score} / {self.question_number}\n"
+            )
 
 
 if __name__ == "__main__":
