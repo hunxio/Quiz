4e795feac4dd3a245c8f79befa11b6c2c982e627
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 17:29:24 +0200

    variable names changed for better readability, formating

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from api import Api
 from quiz import Question
 
+
 def main():
 
-    initialize_api = Api()
-    questions = initialize_api.quiz_request()
+    api = Api()
+    questions = api.quiz_request()
     quiz_list = []
 
     for item in questions:
         quiz_list.append(item)
 
-
-    initialize_question = Question()
-    test = initialize_question.game(quiz_list)
-    print(test)
+    question = Question()
+    game = question.game(quiz_list)
 
 
 if __name__ == "__main__":
