808bbf0e1436ba1148ea0c0cb709be2212faf3a3
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 16:23:59 +0200

    main testing

---
+++ main.py
@@ -0,0 +1,14 @@
+from api import Api
+from quiz import Question
+
+
+initialize_api = Api()
+questions = initialize_api.quiz_request()
+quiz_list = []
+for item in questions:
+    quiz_list.append(item)
+
+
+initialize_question = Question()
+test = initialize_question.test(quiz_list)
+print(test)
