668139c2a78319c152291e191102af72c9a7004a
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 16:23:53 +0200

    quiz init

---
+++ quiz.py
@@ -0,0 +1,13 @@
+from api import Api
+
+class Question:
+    def __init__(self):
+        self.question_number = 0
+        self.correct_answer = 0
+    
+    def test(self, question):
+        return question
+
+
+if __name__ == "__main__":
+    main()
