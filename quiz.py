8e209dcd1039020f9e35b7a178bcb62777059bfa
Huno <136697789+hunxio@users.noreply.github.com>
2024-09-26 17:16:53 +0200

    misstypo fix

--- quiz.py
+++ quiz.py
@@ -7,17 +7,18 @@ class Question:
     
 
     def game(self, question):
-        while question_number < len(question):
+        while self.question_number < len(question):
             text = question[self.question_number]["question"]
             correct_answer = question[self.question_number]["correct_answer"]
             self.question_number += 1
-            user_input = input(f"Q.{self.question_number}")
-            check_answer(user_answer, correct_answer)
+            user_input = input(f"Q.{self.question_number}: {text}")
+            check_answer(user_input, correct_answer)
 
 
     def check_answer(self, user_asnwer, correct_answer):
-        if check_answer.capitalize() == correct_answer:
+        if user_answer.capitalize() == correct_answer:
             self.user_score += 1
+            return f"Good job! Score: {self.user_score} / {self.question_number}"
         else:
             return f"Sorry, wrong answer.\nScore: {self.user_score} / {self.question_number}"
     
